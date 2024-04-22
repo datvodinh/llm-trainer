a932443fc9346e74469ac5cd66beb2c01ca47fd7
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-04-22 20:51:49 +0700

    perf: update training function calling script

--- scripts/train_function_calling.sh
+++ scripts/train_function_calling.sh
@@ -9,6 +9,10 @@ while [[ $# -gt 0 ]]; do
             hf_token="$2"
             shift
             shift ;;
+        --wandb_token)
+            wandb_token="$2"
+            shift
+            shift ;;
         *)
             echo "Unknown option: $1"
             exit 1 ;;
@@ -16,19 +20,20 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Check if required arguments are provided
-if [[ -z "$hf_username" || -z "$hf_token" ]]; then
+if [[ -z "$hf_username" || -z "$hf_token" || -z "$wandb_token" ]]; then
     echo "Error: Missing required arguments."
-    echo "Usage: train_function_calling.sh --hf_username <username> --hf_token <token>"
+    echo "Usage: train_function_calling.sh --hf_username <username> --hf_token <token> --wandb_token <token>"
     exit 1
 fi
 
 
 python -m llm_trainer.function_calling \
-    --model_name "unsloth/llama-3-8b-Instruct-bnb-4bit" \
-    --per_device_train_batch_size 4 \
+    --model_name "unsloth/llama-3-8b-bnb-4bit" \
+    --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 4 \
     --warmup_ratio 0.1 \
-    --max_steps 3000 \
+    --rank 16 \
+    --max_steps 2000 \
     --num_train_epochs 1 \
     --learning_rate 0.00002 \
     --logging_steps 10 \
@@ -36,8 +41,10 @@ python -m llm_trainer.function_calling \
     --weight_decay 0.01 \
     --lr_scheduler_type "cosine" \
     --seed 3407 \
-    --output_dir "llama-3-8b-instruct-funtion-calling" \
+    --output_dir "llama-3-8b-funtion-calling-q4_k_m-gguf" \
     --max_seq_length 2048 \
     --hf_username "$hf_username" \
-    --finetune_model_name "llama-3-8b-instruct-funtion-calling" \
-    --hf_token "$hf_token"
+    --finetune_model_name "llama-3-8b-funtion-calling-q4_k_m-gguf" \
+    --hf_token "$hf_token" \
+    --wandb_token "$wandb_token" \
+    --quantization_method "q4_k_m"
